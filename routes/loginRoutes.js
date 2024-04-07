43c59228b992c4b0964c81bdf99f1171f862d7a0
daskie911 <imevil2302@gmail.com>
2024-04-07 11:44:45 +0200

    Update loginRoutes.js to pass contacts and login session to admin view

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
     if (login === process.env.LOGIN && password === process.env.PASSWORD) {
       req.session.user = login;
       console.log("Done!");
-      res.render("admin", { login });
+      res.render("admin", { contacts, login: req.session.user });
     } else {
       console.log("Wrong login or password!");
       res.render("login", { error: "Wrong login or password!" });
