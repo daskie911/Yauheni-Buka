66d7379e54b3eb42af8cc5583c253fcc931aff5e
daskie911 <imevil2302@gmail.com>
2024-04-22 17:56:47 +0200

    Update mainRoutes.js and index.ejs to add new skills functionality

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
 const Contact = require("../model/Contact");
 const Card = require("../model/Card");
+const Skill = require("../model/Skill");
 const router = new Router();
 
 router.get("/", async (req, res) => {
   try {
     const contact = await Contact.findOne();
     const cards = await Card.find().sort({ _id: -1 });
-    res.render("index", { cards, contact });
+    const skills = await Skill.find().sort({ _id: -1 });
+    res.render("index", { cards, contact, skills });
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong");
