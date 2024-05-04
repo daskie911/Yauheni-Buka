c323668160eb0eaa6c2d773b35f19c29dfd6464e
daskie911 <imevil2302@gmail.com>
2024-05-04 20:58:27 +0200

    Add SendGrid email functionality for contact form

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const Contact = require("../model/Contact");
 const Card = require("../model/Card");
 const Skill = require("../model/Skill");
+const sendGridMail = require("@sendgrid/mail");
 const router = new Router();
+sendGridMail.setApiKey(`${process.env.API_KEY}`);
 
 router.get("/", async (req, res) => {
   try {
@@ -13,6 +15,23 @@ router.get("/", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong");
+  }
+});
+
+router.post("/email", async (req, res) => {
+  try {
+    const { title, email, message } = req.body;
+    console.log(title, email, message);
+    const msg = {
+      to: "darkpunky94@gmail.com",
+      from: email,
+      subject: title,
+      text: message,
+    };
+    sendGridMail.send(msg);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
   }
 });
 
