e1c8225bffff9cdc9d105da14037b3f2867d11c8
daskie911 <imevil2302@gmail.com>
2024-06-04 00:45:40 +0200

    feat: Add SibApiV3Sdk dependency for sending transactional emails

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,20 +1,37 @@
+const { name } = require("ejs");
 const { Router } = require("express");
 const router = new Router();
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-router.post("/email", async (req, res) => {
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.EMAIL_API_KEY;
+
+router.post("/send", async (req, res) => {
+  const apiIntance = new SibApiV3Sdk.TransactionalEmailsApi();
+  const sender = {
+    email: "darkpunky94@gmail.com",
+    name: "Daskie",
+  };
+
+  const receivers = [
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+  ];
   try {
-    const { title, email, message } = req.body;
-    console.log(title, email, message);
-    const msg = {
-      to: "darkpunky94@gmail.com",
-      from: email,
-      subject: title,
-      text: message,
-    };
-    sendGridMail.send(msg);
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
+    const sendEmail = await apiIntance.sendTransacEmail({
+      sender,
+      to: receivers,
+      Subject: req.body.subject,
+      textContent: req.body.message,
+      htmlContent: "<strong>This is a test email from Daskie</strong>",
+    });
+    return res.send({ message: "Email sent successfully sent" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
   }
 });
 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,6 +3,11 @@ const Contact = require("../model/Contact");
 const Card = require("../model/Card");
 const Skill = require("../model/Skill");
 const Review = require("../model/Review");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.EMAIL_API_KEY;
 
 const router = new Router();
 
@@ -19,21 +24,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/email", async (req, res) => {
-  try {
-    const { title, email, message } = req.body;
-    console.log(title, email, message);
-    const msg = {
-      to: "darkpunky94@gmail.com",
-      from: email,
-      subject: title,
-      text: message,
-    };
-    sendGridMail.send(msg);
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
+
 
 module.exports = router;
